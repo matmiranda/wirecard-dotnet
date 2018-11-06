b69109e7d5440d8a109d5f58a1e62f4ed7ef6fbf
Matheus Miranda <mat.miranda@outlook.com>
2018-11-06 19:36:40 -0200

    Update TransparentAccountRequest.cs

--- WirecardCSharp/WirecardCSharp/Models/Request/TransparentAccountRequest.cs
+++ WirecardCSharp/WirecardCSharp/Models/Request/TransparentAccountRequest.cs
@@ -1,15 +1,32 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class TransparentAccountRequest
+    public partial class TransparentAccountRequest
     {
-        public Email email { get; set; }
-        public Person person { get; set; }
-        public string type { get; set; }
-        public bool transparentAccount { get; set; }
-
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Company company { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Email email { get => Email; set => value = Email; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Person person { get => Person; set => value = Person; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string type { get => Type; set => value = Type; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public bool transparentAccount { get => TransparentAccount; set => value = TransparentAccount; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Company company { get => Company; set => value = Company; }
+    }
+    public partial class TransparentAccountRequest
+    {
+        [JsonProperty("email", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Email Email { get; set; }
+        [JsonProperty("person", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Person Person { get; set; }
+        [JsonProperty("type", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Type { get; set; }
+        [JsonProperty("transparentAccount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public bool TransparentAccount { get; set; }
+        [JsonProperty("company", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Company Company { get; set; }
     }
 }

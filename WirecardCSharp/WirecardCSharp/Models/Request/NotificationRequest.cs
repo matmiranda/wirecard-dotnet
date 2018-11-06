13550524e283d3069311daf7408a904a401da14a
Matheus Miranda <mat.miranda@outlook.com>
2018-11-06 19:33:36 -0200

    Update NotificationRequest.cs

--- WirecardCSharp/WirecardCSharp/Models/Request/NotificationRequest.cs
+++ WirecardCSharp/WirecardCSharp/Models/Request/NotificationRequest.cs
@@ -1,11 +1,25 @@
-﻿using System.Collections.Generic;
+using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace WirecardCSharp.Models
 {
-    public class NotificationRequest
+    public partial class NotificationRequest
     {
-        public List<string> events { get; set; }
-        public string target { get; set; }
-        public string media { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public List<string> events { get => Events; set => value = Events; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string target { get => Target; set => value = Target; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string media { get => Media; set => value = Media; }
+    }
+    public partial class NotificationRequest
+    {
+        [JsonProperty("events", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public List<string> Events { get; set; }
+        [JsonProperty("target", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Target { get; set; }
+        [JsonProperty("media", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Media { get; set; }
     }
 }

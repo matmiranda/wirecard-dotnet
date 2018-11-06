017d34f147da22b926a8b4df4257053074303478
Matheus Miranda <mat.miranda@outlook.com>
2018-11-06 19:34:30 -0200

    Update OrderRequest.cs

--- WirecardCSharp/WirecardCSharp/Models/Request/OrderRequest.cs
+++ WirecardCSharp/WirecardCSharp/Models/Request/OrderRequest.cs
@@ -1,13 +1,33 @@
-﻿using System.Collections.Generic;
+using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace WirecardCSharp.Models
 {
-    public class OrderRequest
+    public partial class OrderRequest
     {
-        public string ownId { get; set; }
-        public Amount amount { get; set; }
-        public List<Item> items { get; set; }
-        public Customer customer { get; set; }
-        public List<Receiver> receivers { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string ownId { get => OwnId; set => value = OwnId; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Amount amount { get => Amount; set => value = Amount; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public List<Item> items { get => Items; set => value = Items; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Customer customer { get => Customer; set => value = Customer; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public List<Receiver> receivers { get => Receivers; set => value = Receivers; }
+    }
+    public partial class OrderRequest
+    {
+        [JsonProperty("ownId", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string OwnId { get; set; }
+        [JsonProperty("amount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Amount Amount { get; set; }
+        [JsonProperty("items", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public List<Item> Items { get; set; }
+        [JsonProperty("customer", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Customer Customer { get; set; }
+        [JsonProperty("receivers", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public List<Receiver> Receivers { get; set; }
     }
 }

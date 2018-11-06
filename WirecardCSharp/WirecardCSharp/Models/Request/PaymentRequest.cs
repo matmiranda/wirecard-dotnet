71b45984eff55de178c7fe255709d5017d2e3890
Matheus Miranda <mat.miranda@outlook.com>
2018-11-06 19:34:54 -0200

    Update PaymentRequest.cs

--- WirecardCSharp/WirecardCSharp/Models/Request/PaymentRequest.cs
+++ WirecardCSharp/WirecardCSharp/Models/Request/PaymentRequest.cs
@@ -1,10 +1,28 @@
-﻿namespace WirecardCSharp.Models
+using System;
+using Newtonsoft.Json;
+
+namespace WirecardCSharp.Models
 {
-    public class PaymentRequest
+    public partial class PaymentRequest
     {
-        public int installmentCount { get; set; }
-        public string statementDescriptor { get; set; }
-        public Fundinginstrument fundingInstrument { get; set; }
-        public Device device { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int installmentCount { get => InstallmentCount; set => value = InstallmentCount; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string statementDescriptor { get => StatementDescriptor; set => value = StatementDescriptor; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Fundinginstrument fundingInstrument { get => FundingInstrument; set => value = FundingInstrument; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Device device { get => Device; set => value = Device; }
+    }
+    public partial class PaymentRequest
+    {
+        [JsonProperty("installmentCount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int InstallmentCount { get; set; }
+        [JsonProperty("statementDescriptor", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string StatementDescriptor { get; set; }
+        [JsonProperty("fundingInstrument", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Fundinginstrument FundingInstrument { get; set; }
+        [JsonProperty("device", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Device Device { get; set; }
     }
 }

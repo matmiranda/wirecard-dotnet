a2af17e9252053cc01eaa3ebdd0dd72dba81f41c
Matheus Miranda <mat.miranda@outlook.com>
2018-11-06 19:29:46 -0200

    Update BankAccountRequest.cs

--- WirecardCSharp/WirecardCSharp/Models/Request/BankAccountRequest.cs
+++ WirecardCSharp/WirecardCSharp/Models/Request/BankAccountRequest.cs
@@ -1,13 +1,40 @@
-﻿namespace WirecardCSharp.Models
+using System;
+using Newtonsoft.Json;
+
+namespace WirecardCSharp.Models
 {
-    public class BankAccountRequest
+    public partial class BankAccountRequest
     {
-        public string bankNumber { get; set; }
-        public string agencyNumber { get; set; }
-        public string agencyCheckNumber { get; set; }
-        public string accountNumber { get; set; }
-        public string accountCheckNumber { get; set; }
-        public string @type { get; set; }
-        public Holder holder { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string bankNumber { get => BankNumber; set => value = BankNumber; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string agencyNumber { get => AgencyNumber; set => value = AgencyNumber; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string agencyCheckNumber { get => AgencyCheckNumber; set => value = AgencyCheckNumber; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string accountNumber { get => AccountNumber; set => value = AccountNumber; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string accountCheckNumber { get => AccountCheckNumber; set => value = AccountCheckNumber; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string @type { get => Type; set => value = Type; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Holder holder { get => Holder; set => value = Holder; }
+    }
+    public partial class BankAccountRequest
+    {
+        [JsonProperty("bankNumber", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string BankNumber { get; set; }
+        [JsonProperty("agencyNumber", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string AgencyNumber { get; set; }
+        [JsonProperty("agencyCheckNumber", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string AgencyCheckNumber { get; set; }
+        [JsonProperty("accountNumber", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string AccountNumber { get; set; }
+        [JsonProperty("accountCheckNumber", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string AccountCheckNumber { get; set; }
+        [JsonProperty("type", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Type { get; set; }
+        [JsonProperty("holder", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Holder Holder { get; set; }
     }
 }

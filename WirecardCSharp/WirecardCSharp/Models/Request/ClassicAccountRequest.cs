3db790fdf2b89110b6fc108604735440d7fd9a26
Matheus Miranda <mat.miranda@outlook.com>
2018-11-06 19:30:29 -0200

    Update ClassicAccountRequest.cs

--- WirecardCSharp/WirecardCSharp/Models/Request/ClassicAccountRequest.cs
+++ WirecardCSharp/WirecardCSharp/Models/Request/ClassicAccountRequest.cs
@@ -1,11 +1,32 @@
-﻿namespace WirecardCSharp.Models
+using System;
+using Newtonsoft.Json;
+
+namespace WirecardCSharp.Models
 {
-    public class ClassicAccountRequest
+    public partial class ClassicAccountRequest
     {
-        public Email email { get; set; }
-        public Person person { get; set; }
-        public Company company { get; set; }
-        public Businesssegment businessSegment { get; set; }
-        public string type { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Email email { get => Email; set => value = Email; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Person person { get => Person; set => value = Person; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Company company { get => Company; set => value = Company; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Businesssegment businessSegment { get => BusinessSegment; set => value = BusinessSegment; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string type { get => Type; set => value = Type; }
+    }
+    public partial class ClassicAccountRequest
+    {
+        [JsonProperty("email", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Email Email { get; set; }
+        [JsonProperty("person", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Person Person { get; set; }
+        [JsonProperty("company", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Company Company { get; set; }
+        [JsonProperty("businessSegment", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Businesssegment BusinessSegment { get; set; }
+        [JsonProperty("type", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Type { get; set; }
     }
 }

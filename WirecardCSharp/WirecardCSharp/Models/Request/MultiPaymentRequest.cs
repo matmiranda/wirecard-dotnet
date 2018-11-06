283caf8e532acbc807cf56006d930caca09ac481
Matheus Miranda <mat.miranda@outlook.com>
2018-11-06 19:33:08 -0200

    Update MultiPaymentRequest.cs

--- WirecardCSharp/WirecardCSharp/Models/Request/MultiPaymentRequest.cs
+++ WirecardCSharp/WirecardCSharp/Models/Request/MultiPaymentRequest.cs
@@ -1,8 +1,20 @@
-﻿namespace WirecardCSharp.Models
+using System;
+using Newtonsoft.Json;
+
+namespace WirecardCSharp.Models
 {
-    public class MultiPaymentRequest
+    public partial class MultiPaymentRequest
     {
-        public int installmentCount { get; set; }
-        public Fundinginstrument fundingInstrument { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int installmentCount { get => InstallmentCount; set => value = InstallmentCount; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Fundinginstrument fundingInstrument { get => FundingInstrument; set => value = FundingInstrument; }
+    }
+    public partial class MultiPaymentRequest
+    {
+        [JsonProperty("installmentCount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int InstallmentCount { get; set; }
+        [JsonProperty("fundingInstrument", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Fundinginstrument FundingInstrument { get; set; }
     }
 }

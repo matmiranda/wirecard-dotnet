27cb7240d0caf3e03509bef50b58645f7138cc42
Matheus Miranda <mat.miranda@outlook.com>
2018-11-06 19:35:29 -0200

    Update RefundRequest.cs

--- WirecardCSharp/WirecardCSharp/Models/Request/RefundRequest.cs
+++ WirecardCSharp/WirecardCSharp/Models/Request/RefundRequest.cs
@@ -1,9 +1,24 @@
-﻿namespace WirecardCSharp.Models
+using System;
+using Newtonsoft.Json;
+
+namespace WirecardCSharp.Models
 {
-    public class RefundRequest
+    public partial class RefundRequest
     {
-        public string amount { get; set; }
-        public Refundinginstrument refundingInstrument { get; set; }
-        public Creditcard creditCard { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string amount { get => Amount; set => value = Amount; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Refundinginstrument refundingInstrument { get => RefundingInstrument; set => value = RefundingInstrument; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Creditcard creditCard { get => CreditCard; set => value = CreditCard; }
+    }
+    public partial class RefundRequest
+    {
+        [JsonProperty("amount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Amount { get; set; }
+        [JsonProperty("refundingInstrument", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Refundinginstrument RefundingInstrument { get; set; }
+        [JsonProperty("creditCard", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Creditcard CreditCard { get; set; }
     }
 }

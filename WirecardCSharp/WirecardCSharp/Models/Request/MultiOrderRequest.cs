bc054349b68579ece3f32ba1f90fdd730351e54d
Matheus Miranda <mat.miranda@outlook.com>
2018-11-06 19:32:30 -0200

    Update MultiOrderRequest.cs

--- WirecardCSharp/WirecardCSharp/Models/Request/MultiOrderRequest.cs
+++ WirecardCSharp/WirecardCSharp/Models/Request/MultiOrderRequest.cs
@@ -1,10 +1,21 @@
-﻿using System.Collections.Generic;
+using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace WirecardCSharp.Models
 {
-    public class MultiOrderRequest
+    public partial class MultiOrderRequest
     {
-        public string ownId { get; set; }
-        public List<Order> orders { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string ownId { get => OwnId; set => value = OwnId; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public List<Order> orders { get => Orders; set => value = Orders; }
+    }
+    public partial class MultiOrderRequest
+    {
+        [JsonProperty("ownId", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string OwnId { get; set; }
+        [JsonProperty("orders", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public List<Order> Orders { get; set; }
     }
 }

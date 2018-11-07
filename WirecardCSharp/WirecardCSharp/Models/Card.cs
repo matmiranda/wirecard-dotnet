dc2d1dc3bb20d51edb10ee9be902448427fb0601
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 18:33:59 -0200

    Update Card.cs

--- WirecardCSharp/WirecardCSharp/Models/Card.cs
+++ WirecardCSharp/WirecardCSharp/Models/Card.cs
@@ -1,12 +1,20 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class Card
+    public partial class Card
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string brand { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public bool store { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string brand { get => Brand; set => value = Brand; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public bool store { get => Store; set => value = Store; }
+    }
+    public partial class Card
+    {
+        [JsonProperty("brand", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Brand { get; set; }
+        [JsonProperty("store", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public bool Store { get; set; }
     }
 }

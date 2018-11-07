4120911cb10bca60853dd131b7bb3566ef711de9
Matheus Miranda <mat.miranda@outlook.com>
2018-11-06 23:29:00 -0200

    Update PaymentResponse.cs

--- WirecardCSharp/WirecardCSharp/Models/Response/PaymentResponse.cs
+++ WirecardCSharp/WirecardCSharp/Models/Response/PaymentResponse.cs
@@ -1,27 +1,85 @@
-﻿using System;
+using System;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 
 namespace WirecardCSharp.Models
 {
-    public class PaymentResponse
+    public partial class PaymentResponse
     {
-        public string id { get; set; }
-        public string status { get; set; }
-        public bool delayCapture { get; set; }
-        public Amount amount { get; set; }
-        public int installmentCount { get; set; }
-        public string statementDescriptor { get; set; }
-        public Fundinginstrument fundingInstrument { get; set; }
-        public List<Fee> fees { get; set; }
-        public List<Event> events { get; set; }
-        public List<Receiver> receivers { get; set; }
-        public Device device { get; set; }
-        public _Links _links { get; set; }
-        public DateTime createdAt { get; set; }
-        public DateTime updatedAt { get; set; }
-        public string type { get; set; }
-        public Refundinginstrument refundingInstrument { get; set; }
-        public string description { get; set; }
-        public Cancellationdetails cancellationDetails { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string id { get => Id; set => value = Id; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string status { get => Status; set => value = Status; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public bool delayCapture { get => DelayCapture; set => value = DelayCapture; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Amount amount { get => Amount; set => value = Amount; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int installmentCount { get => InstallmentCount; set => value = InstallmentCount; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string statementDescriptor { get => StatementDescriptor; set => value = StatementDescriptor; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Fundinginstrument fundingInstrument { get => FundingInstrument; set => value = FundingInstrument; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public List<Fee> fees { get => Fees; set => value = Fees; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public List<Event> events { get => Events; set => value = Events; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public List<Receiver> receivers { get => Receivers; set => value = Receivers; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Device device { get => Device; set => value = Device; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public _Links _links { get => _Links; set => value = _Links; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public DateTime createdAt { get => CreatedAt; set => value = CreatedAt; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public DateTime updatedAt { get => UpdatedAt; set => value = UpdatedAt; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string type { get => Type; set => value = Type; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Refundinginstrument refundingInstrument { get => RefundingInstrument; set => value = RefundingInstrument; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string description { get => Description; set => value = Description; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Cancellationdetails cancellationDetails { get => CancellationDetails; set => value = CancellationDetails; }
+    }
+    public partial class PaymentResponse
+    {
+        [JsonProperty("id", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Id { get; set; }
+        [JsonProperty("status", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Status { get; set; }
+        [JsonProperty("delayCapture", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public bool DelayCapture { get; set; }
+        [JsonProperty("amount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Amount Amount { get; set; }
+        [JsonProperty("installmentCount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int InstallmentCount { get; set; }
+        [JsonProperty("statementDescriptor", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string StatementDescriptor { get; set; }
+        [JsonProperty("fundingInstrument", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Fundinginstrument FundingInstrument { get; set; }
+        [JsonProperty("fees", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public List<Fee> Fees { get; set; }
+        [JsonProperty("events", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public List<Event> Events { get; set; }
+        [JsonProperty("receivers", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public List<Receiver> Receivers { get; set; }
+        [JsonProperty("device", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Device Device { get; set; }
+        [JsonProperty("_links", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public _Links _Links { get; set; }
+        [JsonProperty("createdAt", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public DateTime CreatedAt { get; set; }
+        [JsonProperty("updatedAt", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public DateTime UpdatedAt { get; set; }
+        [JsonProperty("type", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Type { get; set; }
+        [JsonProperty("refundingInstrument", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Refundinginstrument RefundingInstrument { get; set; }
+        [JsonProperty("description", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Description { get; set; }
+        [JsonProperty("cancellationDetails", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Cancellationdetails CancellationDetails { get; set; }
     }
 }

ec93a38687269b24eca90f96a3060dbe02570ccd
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 16:56:24 -0200

    Update Phone.cs

--- WirecardCSharp/WirecardCSharp/Models/Phone.cs
+++ WirecardCSharp/WirecardCSharp/Models/Phone.cs
@@ -1,14 +1,24 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class Phone
+    public partial class Phone
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string countryCode { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string areaCode { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string number { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string countryCode { get => CountryCode; set => value = CountryCode; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string areaCode { get => AreaCode; set => value = AreaCode; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string number { get => Number; set => value = Number; }
+    }
+    public partial class Phone
+    {
+        [JsonProperty("countryCode", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string CountryCode { get; set; }
+        [JsonProperty("areaCode", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string AreaCode { get; set; }
+        [JsonProperty("number", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Number { get; set; }
     }
 }

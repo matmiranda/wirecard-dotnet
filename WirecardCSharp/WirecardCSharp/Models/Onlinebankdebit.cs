6dcaf0bce572b5ecac08fc76f2d7f44f23abe5c0
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 20:08:46 -0200

    Update Onlinebankdebit.cs

--- WirecardCSharp/WirecardCSharp/Models/Onlinebankdebit.cs
+++ WirecardCSharp/WirecardCSharp/Models/Onlinebankdebit.cs
@@ -1,14 +1,24 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class Onlinebankdebit
+    public partial class Onlinebankdebit
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string bankNumber { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string expirationDate { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string bankName { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string bankNumber { get => BankNumber; set => value = BankNumber; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string expirationDate { get => ExpirationDate; set => value = ExpirationDate; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string bankName { get => BankName; set => value = BankName; }
+    }
+    public partial class Onlinebankdebit
+    {
+        [JsonProperty("bankNumber", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string BankNumber { get; set; }
+        [JsonProperty("expirationDate", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string ExpirationDate { get; set; }
+        [JsonProperty("bankName", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string BankName { get; set; }
     }
 }

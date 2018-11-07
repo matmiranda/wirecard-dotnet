268ea3baa3e0acadaba18e2836aa2cdeb707172b
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 19:49:10 -0200

    Update Identitydocument.cs

--- WirecardCSharp/WirecardCSharp/Models/Identitydocument.cs
+++ WirecardCSharp/WirecardCSharp/Models/Identitydocument.cs
@@ -1,16 +1,28 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class Identitydocument
+    public partial class Identitydocument
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string type { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string number { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string issuer { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string issueDate { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string type { get => Type; set => value = Type; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string number { get => Number; set => value = Number; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string issuer { get => Issuer; set => value = Issuer; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string issueDate { get => IssueDate; set => value = IssueDate; }
+    }
+    public partial class Identitydocument
+    {
+        [JsonProperty("type", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Type { get; set; }
+        [JsonProperty("number", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Number { get; set; }
+        [JsonProperty("issuer", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Issuer { get; set; }
+        [JsonProperty("issueDate", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string IssueDate { get; set; }
     }
 }

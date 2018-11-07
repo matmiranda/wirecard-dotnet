0b4a028fca8b6906255c1b4c08ce3401225af953
Matheus Miranda <mat.miranda@outlook.com>
2018-11-06 23:25:11 -0200

    Update CustomerResponse.cs

--- WirecardCSharp/WirecardCSharp/Models/Response/CustomerResponse.cs
+++ WirecardCSharp/WirecardCSharp/Models/Response/CustomerResponse.cs
@@ -1,39 +1,133 @@
 using System;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 
 namespace WirecardCSharp.Models
 {
-    public class CustomerResponse
+    public partial class CustomerResponse
     {
-        public string id { get; set; }
-        public string ownId { get; set; }
-        public string fullname { get; set; }
-        public DateTime createdAt { get; set; }
-        public string birthDate { get; set; }
-        public string email { get; set; }
-        public Phone phone { get; set; }
-        public Taxdocument taxDocument { get; set; }
-        public Shippingaddress shippingAddress { get; set; }
-        public _Links _links { get; set; }
-        public string status { get; set; }
-        public List<Event> events { get; set; }
-        public Amount amount { get; set; }
-        public string type { get; set; }
-        public Refundinginstrument refundingInstrument { get; set; }
-        public Creditcard creditCard { get; set; }
-        public Card card { get; set; }
-        public string method { get; set; }
-        public string platform { get; set; }
-        public DateTime updatedAt { get; set; }
-        public List<Item> items { get; set; }
-        public List<Address> addresses { get; set; }
-        public Customer customer { get; set; }
-        public object[] payments { get; set; }
-        public object[] escrows { get; set; }
-        public object[] refunds { get; set; }
-        public object[] entries { get; set; }
-        public List<Receiver> receivers { get; set; }
-        public Fundinginstrument fundingInstrument { get; set; }
-        public List<Fundinginstrument> fundingInstruments { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string id { get => Id; set => value = Id; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string ownId { get => OwnId; set => value = OwnId; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string fullname { get => FullName; set => value = FullName; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public DateTime createdAt { get => CreatedAt; set => value = CreatedAt; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string birthDate { get => BirthDate; set => value = BirthDate; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string email { get => Email; set => value = Email; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Phone phone { get => Phone; set => value = Phone; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Taxdocument taxDocument { get => TaxDocument; set => value = TaxDocument; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Shippingaddress shippingAddress { get => ShippingAddress; set => value = ShippingAddress; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public _Links _links { get => _Links; set => value = _Links; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string status { get => Status; set => value = Status; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public List<Event> events { get => Events; set => value = Events; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Amount amount { get => Amount; set => value = Amount; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string type { get => Type; set => value = Type; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Refundinginstrument refundingInstrument { get => RefundingInstrument; set => value = RefundingInstrument; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Creditcard creditCard { get => CreditCard; set => value = CreditCard; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Card card { get => Card; set => value = Card; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string method { get => Method; set => value = Method; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string platform { get => Platform; set => value = Platform; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public DateTime updatedAt { get => UpdatedAt; set => value = UpdatedAt; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public List<Item> items { get => Items; set => value = Items; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public List<Address> addresses { get => Addresses; set => value = Addresses; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Customer customer { get => Customer; set => value = Customer; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public object[] payments { get => Payments; set => value = Payments; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public object[] escrows { get => Escrows; set => value = Escrows; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public object[] refunds { get => Refunds; set => value = Refunds; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public object[] entries { get => Entries; set => value = Entries; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public List<Receiver> receivers { get => Receivers; set => value = Receivers; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Fundinginstrument fundingInstrument { get => FundingInstrument; set => value = FundingInstrument; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public List<Fundinginstrument> fundingInstruments { get => FundingInstruments; set => value = FundingInstruments; }
+    }
+    public partial class CustomerResponse
+    {
+        [JsonProperty("id", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Id { get; set; }
+        [JsonProperty("ownId", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string OwnId { get; set; }
+        [JsonProperty("fullname", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string FullName { get; set; }
+        [JsonProperty("createdAt", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public DateTime CreatedAt { get; set; }
+        [JsonProperty("birthDate", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string BirthDate { get; set; }
+        [JsonProperty("email", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Email { get; set; }
+        [JsonProperty("phone", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Phone Phone { get; set; }
+        [JsonProperty("taxDocument", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Taxdocument TaxDocument { get; set; }
+        [JsonProperty("shippingAddress", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Shippingaddress ShippingAddress { get; set; }
+        [JsonProperty("_links", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public _Links _Links { get; set; }
+        [JsonProperty("status", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Status { get; set; }
+        [JsonProperty("events", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public List<Event> Events { get; set; }
+        [JsonProperty("amount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Amount Amount { get; set; }
+        [JsonProperty("type", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Type { get; set; }
+        [JsonProperty("refundingInstrument", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Refundinginstrument RefundingInstrument { get; set; }
+        [JsonProperty("creditCard", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Creditcard CreditCard { get; set; }
+        [JsonProperty("card", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Card Card { get; set; }
+        [JsonProperty("method", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Method { get; set; }
+        [JsonProperty("platform", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Platform { get; set; }
+        [JsonProperty("updatedAt", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public DateTime UpdatedAt { get; set; }
+        [JsonProperty("items", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public List<Item> Items { get; set; }
+        [JsonProperty("addresses", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public List<Address> Addresses { get; set; }
+        [JsonProperty("customer", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Customer Customer { get; set; }
+        [JsonProperty("payments", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public object[] Payments { get; set; }
+        [JsonProperty("escrows", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public object[] Escrows { get; set; }
+        [JsonProperty("refunds", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public object[] Refunds { get; set; }
+        [JsonProperty("entries", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public object[] Entries { get; set; }
+        [JsonProperty("receivers", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public List<Receiver> Receivers { get; set; }
+        [JsonProperty("fundingInstrument", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Fundinginstrument FundingInstrument { get; set; }
+        [JsonProperty("fundingInstruments", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public List<Fundinginstrument> FundingInstruments { get; set; }
     }
 }

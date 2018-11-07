8ddf487015b4da535a08a8187a823c3fb996acf5
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 19:51:32 -0200

    Update Installment.cs

--- WirecardCSharp/WirecardCSharp/Models/Installment.cs
+++ WirecardCSharp/WirecardCSharp/Models/Installment.cs
@@ -1,17 +1,29 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 
 namespace WirecardCSharp.Models
 {
-    public class Installment
+    public partial class Installment
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int addition { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public List<int> quantity { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int amount { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int number { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int addition { get => Addition; set => value = Addition; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public List<int> quantity { get => Quantity; set => value = Quantity; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int amount { get => Amount; set => value = Amount; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int number { get => Number; set => value = Number; }
+    }
+    public partial class Installment
+    {
+        [JsonProperty("addition", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int Addition { get; set; }
+        [JsonProperty("quantity", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public List<int> Quantity { get; set; }
+        [JsonProperty("amount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int Amount { get; set; }
+        [JsonProperty("number", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int Number { get; set; }
     }
 }

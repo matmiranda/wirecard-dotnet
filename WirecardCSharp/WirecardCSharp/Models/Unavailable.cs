c7bc93109f11731d8e64cc325caa54e3245ca4c9
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 16:49:42 -0200

    Update Unavailable.cs

--- WirecardCSharp/WirecardCSharp/Models/Unavailable.cs
+++ WirecardCSharp/WirecardCSharp/Models/Unavailable.cs
@@ -1,12 +1,20 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
+using System;
 
 namespace WirecardCSharp.Models
 {
-    public class Unavailable
+    public partial class Unavailable
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int amount { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string currency { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int amount { get => Amount; set => value = Amount; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string currency { get => Currency; set => value = Currency; }
+    }
+    public partial class Unavailable
+    {
+        [JsonProperty("amount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int Amount { get; set; }
+        [JsonProperty("currency", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Currency { get; set; }
     }
 }

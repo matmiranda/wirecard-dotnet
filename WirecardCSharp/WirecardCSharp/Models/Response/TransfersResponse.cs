e792ca63f02478c8aaea33936977ce9a8fe9ba7a
Matheus Miranda <mat.miranda@outlook.com>
2018-11-06 23:31:05 -0200

    Update TransfersResponse.cs

--- WirecardCSharp/WirecardCSharp/Models/Response/TransfersResponse.cs
+++ WirecardCSharp/WirecardCSharp/Models/Response/TransfersResponse.cs
@@ -1,11 +1,25 @@
-﻿using System.Collections.Generic;
+using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace WirecardCSharp.Models
 {
-    public class TransfersResponse
+    public partial class TransfersResponse
     {
-        public Summary summary { get; set; }
-        public List<Transfer> transfers { get; set; }
-        public _Links _links { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Summary summary { get => Summary; set => value = Summary; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public List<Transfer> transfers { get => Transfers; set => value = Transfers; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public _Links _links { get => _Links; set => value = _Links; }
+    }
+    public partial class TransfersResponse
+    {
+        [JsonProperty("summary", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Summary Summary { get; set; }
+        [JsonProperty("transfers", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public List<Transfer> Transfers { get; set; }
+        [JsonProperty("_links", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public _Links _Links { get; set; }
     }
 }

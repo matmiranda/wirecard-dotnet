498a9bb0bbb2001f9ea7cd132cf2b9a845caecaf
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 19:02:39 -0200

    Update Device.cs

--- WirecardCSharp/WirecardCSharp/Models/Device.cs
+++ WirecardCSharp/WirecardCSharp/Models/Device.cs
@@ -1,16 +1,28 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class Device
+    public partial class Device
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string ip { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Geolocation geolocation { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string userAgent { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string fingerprint { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string ip { get => Ip; set => value = Ip; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Geolocation geolocation { get => Geolocation; set => value = Geolocation; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string userAgent { get => UserAgent; set => value = UserAgent; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string fingerprint { get => Fingerprint; set => value = Fingerprint; }
+    }
+    public partial class Device
+    {
+        [JsonProperty("ip", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Ip { get; set; }
+        [JsonProperty("geolocation", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Geolocation Geolocation { get; set; }
+        [JsonProperty("userAgent", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string UserAgent { get; set; }
+        [JsonProperty("fingerprint", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Fingerprint { get; set; }
     }
 }

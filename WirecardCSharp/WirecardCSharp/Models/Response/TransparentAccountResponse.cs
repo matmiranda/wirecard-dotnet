dadb83be7dd22c76eda40d4edd021beda2afcd88
Matheus Miranda <mat.miranda@outlook.com>
2018-11-06 23:31:25 -0200

    Update TransparentAccountResponse.cs

--- WirecardCSharp/WirecardCSharp/Models/Response/TransparentAccountResponse.cs
+++ WirecardCSharp/WirecardCSharp/Models/Response/TransparentAccountResponse.cs
@@ -1,18 +1,52 @@
-﻿using System;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class TransparentAccountResponse
+    public partial class TransparentAccountResponse
     {
-        public string id { get; set; }
-        public string login { get; set; }
-        public string accessToken { get; set; }
-        public string channelId { get; set; }
-        public string type { get; set; }
-        public bool transparentAccount { get; set; }
-        public Email email { get; set; }
-        public Person person { get; set; }
-        public DateTime createdAt { get; set; }
-        public _Links _links { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string id { get => Id; set => value = Id; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string login { get => Login; set => value = Login; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string accessToken { get => AccessToken; set => value = AccessToken; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string channelId { get => ChannelId; set => value = ChannelId; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string type { get => Type; set => value = Type; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public bool transparentAccount { get => TransparentAccount; set => value = TransparentAccount; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Email email { get => Email; set => value = Email; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Person person { get => Person; set => value = Person; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public DateTime createdAt { get => CreatedAt; set => value = CreatedAt; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public _Links _links { get => _Links; set => value = _Links; }
+    }
+    public partial class TransparentAccountResponse
+    {
+        [JsonProperty("id", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Id { get; set; }
+        [JsonProperty("login", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Login { get; set; }
+        [JsonProperty("accessToken", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string AccessToken { get; set; }
+        [JsonProperty("channelId", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string ChannelId { get; set; }
+        [JsonProperty("type", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Type { get; set; }
+        [JsonProperty("transparentAccount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public bool TransparentAccount { get; set; }
+        [JsonProperty("email", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Email Email { get; set; }
+        [JsonProperty("person", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Person Person { get; set; }
+        [JsonProperty("createdAt", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public DateTime CreatedAt { get; set; }
+        [JsonProperty("_links", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public _Links _Links { get; set; }
     }
 }

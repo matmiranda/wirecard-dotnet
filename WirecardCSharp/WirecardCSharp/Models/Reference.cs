ccfabc56dd9036c8dc565749cf2382bb632f3484
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 16:54:20 -0200

    Update Reference.cs

--- WirecardCSharp/WirecardCSharp/Models/Reference.cs
+++ WirecardCSharp/WirecardCSharp/Models/Reference.cs
@@ -1,12 +1,20 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class Reference
+    public partial class Reference
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string value { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string type { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string value { get => Value; set => value = Value; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string type { get => Type; set => value = Type; }
+    }
+    public partial class Reference
+    {
+        [JsonProperty("value", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Value { get; set; }
+        [JsonProperty("type", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Type { get; set; }
     }
 }

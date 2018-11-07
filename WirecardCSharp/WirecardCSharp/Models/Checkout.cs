a2a7bf564015a0fc6ea6a1b2cdfb77c77a222de9
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 18:36:48 -0200

    Update Checkout.cs

--- WirecardCSharp/WirecardCSharp/Models/Checkout.cs
+++ WirecardCSharp/WirecardCSharp/Models/Checkout.cs
@@ -1,16 +1,28 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class Checkout
+    public partial class Checkout
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Paycheckout payCheckout { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Paycreditcard payCreditCard { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Payboleto payBoleto { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Payonlinebankdebititau payOnlineBankDebitItau { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Paycheckout payCheckout { get => PayCheckout; set => value = PayCheckout; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Paycreditcard payCreditCard { get => PayCreditCard; set => value = PayCreditCard; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Payboleto payBoleto { get => PayBoleto; set => value = PayBoleto; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Payonlinebankdebititau payOnlineBankDebitItau { get => PayOnlineBankDebitItau; set => value = PayOnlineBankDebitItau; }
+    }
+    public partial class Checkout
+    {
+        [JsonProperty("payCheckout", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Paycheckout PayCheckout { get; set; }
+        [JsonProperty("payCreditCard", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Paycreditcard PayCreditCard { get; set; }
+        [JsonProperty("payBoleto", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Payboleto PayBoleto { get; set; }
+        [JsonProperty("payOnlineBankDebitItau", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Payonlinebankdebititau PayOnlineBankDebitItau { get; set; }
     }
 }

dcc73bd094abfa59741e1cd3ffda09401780ea91
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 16:51:57 -0200

    Update Subtotals.cs

--- WirecardCSharp/WirecardCSharp/Models/Subtotals.cs
+++ WirecardCSharp/WirecardCSharp/Models/Subtotals.cs
@@ -1,16 +1,28 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class Subtotals
+    public partial class Subtotals
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int shipping { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int addition { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int discount { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int items { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int shipping { get => Shipping; set => value = Shipping; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int addition { get => Addition; set => value = Addition; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int discount { get => Discount; set => value = Discount; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int items { get => Items; set => value = Items; }
+    }
+    public partial class Subtotals
+    {
+        [JsonProperty("shipping", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int Shipping { get; set; }
+        [JsonProperty("addition", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int Addition { get; set; }
+        [JsonProperty("discount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int Discount { get; set; }
+        [JsonProperty("items", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int Items { get; set; }
     }
 }

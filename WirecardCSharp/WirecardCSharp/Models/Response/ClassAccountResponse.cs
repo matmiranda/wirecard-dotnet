4e42038abd689d1cc439f0eb08eaaaa2232ea87b
Matheus Miranda <mat.miranda@outlook.com>
2018-11-06 23:24:27 -0200

    Update ClassAccountResponse.cs

--- WirecardCSharp/WirecardCSharp/Models/Response/ClassAccountResponse.cs
+++ WirecardCSharp/WirecardCSharp/Models/Response/ClassAccountResponse.cs
@@ -1,25 +1,80 @@
-﻿using System;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class ClassAccountResponse
+    public partial class ClassAccountResponse
     {
-        public string id { get; set; }
-        public string accessToken { get; set; }
-        public string channelId { get; set; }
-        public string type { get; set; }
-        public bool transparentAccount { get; set; }
-        public DateTime createdAt { get; set; }
-        public Person person { get; set; }
-        public Email email { get; set; }
-        public _Links _links { get; set; }
-        public string login { get; set; }
-        public Businesssegment businessSegment { get; set; }
-        public string feeType { get; set; }
-        public int monthlyRevenueId { get; set; }
-        public string softDescriptor { get; set; }
-        public bool limited { get; set; }
-        public bool verified { get; set; }
-        public Company company { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string id { get => Id; set => value = Id; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string accessToken { get => AccessToken; set => value = AccessToken; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string channelId { get => ChannelId; set => value = ChannelId; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string type { get => Type; set => value = Type; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public bool transparentAccount { get => TransparentAccount; set => value = TransparentAccount; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public DateTime createdAt { get => CreatedAt; set => value = CreatedAt; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Person person { get => Person; set => value = Person; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Email email { get => Email; set => value = Email; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public _Links _links { get => _Links; set => value = _Links; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string login { get => Login; set => value = Login; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Businesssegment businessSegment { get => BusinessSegment; set => value = BusinessSegment; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string feeType { get => FeeType; set => value = FeeType; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int monthlyRevenueId { get => MonthlyRevenueId; set => value = MonthlyRevenueId; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string softDescriptor { get => SoftDescriptor; set => value = SoftDescriptor; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public bool limited { get => Limited; set => value = Limited; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public bool verified { get => Verified; set => value = Verified; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Company company { get => Company; set => value = Company; }
+    }
+    public partial class ClassAccountResponse
+    {
+        [JsonProperty("id", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Id { get; set; }
+        [JsonProperty("accessToken", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string AccessToken { get; set; }
+        [JsonProperty("channelId", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string ChannelId { get; set; }
+        [JsonProperty("type", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Type { get; set; }
+        [JsonProperty("transparentAccount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public bool TransparentAccount { get; set; }
+        [JsonProperty("createdAt", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public DateTime CreatedAt { get; set; }
+        [JsonProperty("person", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Person Person { get; set; }
+        [JsonProperty("email", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Email Email { get; set; }
+        [JsonProperty("_links", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public _Links _Links { get; set; }
+        [JsonProperty("login", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Login { get; set; }
+        [JsonProperty("businessSegment", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Businesssegment BusinessSegment { get; set; }
+        [JsonProperty("feeType", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string FeeType { get; set; }
+        [JsonProperty("monthlyRevenueId", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int MonthlyRevenueId { get; set; }
+        [JsonProperty("softDescriptor", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string SoftDescriptor { get; set; }
+        [JsonProperty("limited", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public bool Limited { get; set; }
+        [JsonProperty("verified", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public bool Verified { get; set; }
+        [JsonProperty("company", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Company Company { get; set; }
     }
 }

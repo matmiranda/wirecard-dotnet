26005615e72b5e018fa0029b15e27bcfdb3dfc91
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 16:51:36 -0200

    Update Summary.cs

--- WirecardCSharp/WirecardCSharp/Models/Summary.cs
+++ WirecardCSharp/WirecardCSharp/Models/Summary.cs
@@ -1,12 +1,20 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class Summary
+    public partial class Summary
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int count { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int amount { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int count { get => Count; set => value = Count; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int amount { get => Amount; set => value = Amount; }
+    }
+    public partial class Summary
+    {
+        [JsonProperty("count", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int Count { get; set; }
+        [JsonProperty("amount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int Amount { get; set; }
     }
 }

1e99e1a3efe72147d6818950da36944f2ee62530
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 16:53:33 -0200

    Update Refundinginstrument.cs

--- WirecardCSharp/WirecardCSharp/Models/Refundinginstrument.cs
+++ WirecardCSharp/WirecardCSharp/Models/Refundinginstrument.cs
@@ -1,16 +1,28 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class Refundinginstrument
+    public partial class Refundinginstrument
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Creditcard creditCard { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string method { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Moipaccount moipAccount { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Bankaccount bankAccount { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Creditcard creditCard { get => CreditCard; set => value = CreditCard; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string method { get => Method; set => value = Method; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Moipaccount moipAccount { get => MoipAccount; set => value = MoipAccount; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Bankaccount bankAccount { get => BankAccount; set => value = BankAccount; }
+    }
+    public partial class Refundinginstrument
+    {
+        [JsonProperty("creditCard", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Creditcard CreditCard { get; set; }
+        [JsonProperty("method", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Method { get; set; }
+        [JsonProperty("moipAccount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Moipaccount MoipAccount { get; set; }
+        [JsonProperty("bankAccount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Bankaccount BankAccount { get; set; }
     }
 }

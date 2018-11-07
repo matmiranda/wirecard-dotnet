e951b722fd5c2fdeb091c5e0893cd87f844d903a
Matheus Miranda <mat.miranda@outlook.com>
2018-11-06 23:27:59 -0200

    Update NotificationResponse.cs

--- WirecardCSharp/WirecardCSharp/Models/Response/NotificationResponse.cs
+++ WirecardCSharp/WirecardCSharp/Models/Response/NotificationResponse.cs
@@ -1,13 +1,33 @@
-﻿using System.Collections.Generic;
+using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace WirecardCSharp.Models
 {
-    public class NotificationResponse
+    public partial class NotificationResponse
     {
-        public List<string> events { get; set; }
-        public string target { get; set; }
-        public string media { get; set; }
-        public string token { get; set; }
-        public string id { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public List<string> events { get => Events; set => value = Events; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string target { get => Target; set => value = Target; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string media { get => Media; set => value = Media; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string token { get => Token; set => value = Token; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string id { get => Id; set => value = Id; }
+    }
+    public partial class NotificationResponse
+    {
+        [JsonProperty("events", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public List<string> Events { get; set; }
+        [JsonProperty("target", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Target { get; set; }
+        [JsonProperty("media", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Media { get; set; }
+        [JsonProperty("token", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Token { get; set; }
+        [JsonProperty("id", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Id { get; set; }
     }
 }

419f2957f047b8ccf55f33a639c5cae234b5385d
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 20:02:32 -0200

    Update Moipaccount.cs

--- WirecardCSharp/WirecardCSharp/Models/Moipaccount.cs
+++ WirecardCSharp/WirecardCSharp/Models/Moipaccount.cs
@@ -1,18 +1,32 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class Moipaccount
+    public partial class Moipaccount
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string id { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string login { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string fullname { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string account { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string email { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string id { get => Id; set => value = Id; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string login { get => Login; set => value = Login; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string fullname { get => FullName; set => value = FullName; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string account { get => Account; set => value = Account; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string email { get => Email; set => value = Email; }
+    }
+    public partial class Moipaccount
+    {
+        [JsonProperty("id", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Id { get; set; }
+        [JsonProperty("login", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Login { get; set; }
+        [JsonProperty("fullname", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string FullName { get; set; }
+        [JsonProperty("account", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Account { get; set; }
+        [JsonProperty("email", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Email { get; set; }
     }
 }

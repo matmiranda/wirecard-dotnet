849f34c2fac43e5980417c5327f5d57fed60e398
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 18:43:31 -0200

    Update Company.cs

--- WirecardCSharp/WirecardCSharp/Models/Company.cs
+++ WirecardCSharp/WirecardCSharp/Models/Company.cs
@@ -1,26 +1,48 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class Company
+    public partial class Company
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string name { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string businessName { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string openingDate { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Taxdocument taxDocument { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Mainactivity mainActivity { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Phone phone { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Address address { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string monthlyRevenue { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string constitutionType { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string name { get => Name; set => value = Name; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string businessName { get => BusinessName; set => value = BusinessName; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string openingDate { get => OpeningDate; set => value = OpeningDate; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Taxdocument taxDocument { get => TaxDocument; set => value = TaxDocument; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Mainactivity mainActivity { get => MainActivity; set => value = MainActivity; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Phone phone { get => Phone; set => value = Phone; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Address address { get => Address; set => value = Address; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string monthlyRevenue { get => MonthlyRevenue; set => value = MonthlyRevenue; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string constitutionType { get => ConstitutionType; set => value = ConstitutionType; }
+    }
+    public partial class Company
+    {
+        [JsonProperty("name", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Name { get; set; }
+        [JsonProperty("businessName", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string BusinessName { get; set; }
+        [JsonProperty("openingDate", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string OpeningDate { get; set; }
+        [JsonProperty("taxDocument", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Taxdocument TaxDocument { get; set; }
+        [JsonProperty("mainActivity", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Mainactivity MainActivity { get; set; }
+        [JsonProperty("phone", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Phone Phone { get; set; }
+        [JsonProperty("address", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Address Address { get; set; }
+        [JsonProperty("monthlyRevenue", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string MonthlyRevenue { get; set; }
+        [JsonProperty("constitutionType", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string ConstitutionType { get; set; }
     }
 }

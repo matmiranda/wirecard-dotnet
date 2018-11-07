96421b6cf9392c2ed6e4593a72de9aa9d1cb3cca
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 18:17:12 -0200

    Update Basicauth.cs

--- WirecardCSharp/WirecardCSharp/Models/Basicauth.cs
+++ WirecardCSharp/WirecardCSharp/Models/Basicauth.cs
@@ -1,12 +1,20 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class Basicauth
+    public partial class Basicauth
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string secret { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string token { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string secret { get => Secret; set => value = Secret; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string token { get => Token; set => value = Token; }
+    }
+    public partial class Basicauth
+    {
+        [JsonProperty("secret", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Secret { get; set; }
+        [JsonProperty("token", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Token { get; set; }
     }
 }

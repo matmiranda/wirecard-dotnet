94e0f3f42420e949b186f2d5069d67f664c90248
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 18:31:55 -0200

    Update Cancellationdetails.cs

--- WirecardCSharp/WirecardCSharp/Models/Cancellationdetails.cs
+++ WirecardCSharp/WirecardCSharp/Models/Cancellationdetails.cs
@@ -1,14 +1,24 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class Cancellationdetails
+    public partial class Cancellationdetails
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string code { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string description { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string cancelledBy { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string code { get => Code; set => value = Code; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string description { get => Description; set => value = Description; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string cancelledBy { get => CancelledBy; set => value = CancelledBy; }
+    }
+    public partial class Cancellationdetails
+    {
+        [JsonProperty("code", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Code { get; set; }
+        [JsonProperty("description", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Description { get; set; }
+        [JsonProperty("cancelledBy", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string CancelledBy { get; set; }
     }
 }

deb27653713e25fe6d0335abb0864cb253253658
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 18:29:27 -0200

    Update Businesssegment.cs

--- WirecardCSharp/WirecardCSharp/Models/Businesssegment.cs
+++ WirecardCSharp/WirecardCSharp/Models/Businesssegment.cs
@@ -1,10 +1,16 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class Businesssegment
+    public partial class Businesssegment
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int id { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int id { get => Id; set => value = Id; }
+    }
+    public partial class Businesssegment
+    {
+        [JsonProperty("id", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int Id { get; set; }
     }
 }

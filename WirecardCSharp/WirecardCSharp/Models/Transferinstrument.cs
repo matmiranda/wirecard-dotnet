f3c5225e196a8732e7fac7bf0bf6c1dcd0a4033b
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 16:50:11 -0200

    Update Transferinstrument.cs

--- WirecardCSharp/WirecardCSharp/Models/Transferinstrument.cs
+++ WirecardCSharp/WirecardCSharp/Models/Transferinstrument.cs
@@ -1,14 +1,24 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class Transferinstrument
+    public partial class Transferinstrument
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string method { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Bankaccount bankAccount { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Moipaccount moipAccount { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string method { get => Method; set => value = Method; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Bankaccount bankAccount { get => BankAccount; set => value = BankAccount; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Moipaccount moipAccount { get => MoipAccount; set => value = MoipAccount; }
+    }
+    public partial class Transferinstrument
+    {
+        [JsonProperty("method", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Method { get; set; }
+        [JsonProperty("bankAccount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Bankaccount BankAccount { get; set; }
+        [JsonProperty("moipAccount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Moipaccount MoipAccount { get; set; }
     }
 }

2cf6661967be1f73ef50d7043cff0cb9727c7773
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 16:53:57 -0200

    Update Refund.cs

--- WirecardCSharp/WirecardCSharp/Models/Refund.cs
+++ WirecardCSharp/WirecardCSharp/Models/Refund.cs
@@ -1,26 +1,45 @@
-﻿using System;
+using System;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 
 namespace WirecardCSharp.Models
 {
-    public class Refund
+    public partial class Refund
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string id { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string status { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string type { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Amount amount { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Refundinginstrument refundingInstrument { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public List<Event> events { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public _Links _links { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public DateTime createdAt { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string id { get => Id; set => value = Id; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string status { get => Status; set => value = Status; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string type { get => Type; set => value = Type; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Amount amount { get => Amount; set => value = Amount; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Refundinginstrument refundingInstrument {get => RefundingInstrument; set => value = RefundingInstrument; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public List<Event> events { get => Events; set => value = Events; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public _Links _links { get => _Links; set => value = _Links; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public DateTime createdAt { get => CreatedAt; set => value = CreatedAt; }
+    }
+    public partial class Refund
+    {
+        [JsonProperty("id", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Id { get; set; }
+        [JsonProperty("status", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Status { get; set; }
+        [JsonProperty("type", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Type { get; set; }
+        [JsonProperty("amount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Amount Amount { get; set; }
+        [JsonProperty("refundingInstrument", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Refundinginstrument RefundingInstrument { get; set; }
+        [JsonProperty("events", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public List<Event> Events { get; set; }
+        [JsonProperty("_links", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public _Links _Links { get; set; }
+        [JsonProperty("createdAt", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public DateTime CreatedAt { get; set; }
     }
 }

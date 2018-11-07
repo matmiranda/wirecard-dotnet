ed75b53731e0722bed9d74b01845a0eaec214f97
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 19:38:53 -0200

    Update Future.cs

--- WirecardCSharp/WirecardCSharp/Models/Future.cs
+++ WirecardCSharp/WirecardCSharp/Models/Future.cs
@@ -1,12 +1,20 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class Future
+    public partial class Future
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int amount { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string currency { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int amount { get => Amount; set => value = Amount; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")])]
+        public string currency { get => Currency; set => value = Currency; }
+    }
+    public partial class Future
+    {
+        [JsonProperty("amount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int Amount { get; set; }
+        [JsonProperty("currency", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Currency { get; set; }
     }
 }

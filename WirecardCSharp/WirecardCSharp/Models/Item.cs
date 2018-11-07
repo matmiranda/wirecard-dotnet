244a234cc4d2cff9f1c4f52f61a5491d688e25a4
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 19:54:19 -0200

    Update Item.cs

--- WirecardCSharp/WirecardCSharp/Models/Item.cs
+++ WirecardCSharp/WirecardCSharp/Models/Item.cs
@@ -1,18 +1,32 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class Item
+    public partial class Item
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string product { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string category { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int quantity { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string detail { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int price { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string product { get => Product; set => value = Product; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string category { get => Category; set => value = Category; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int quantity { get => Quantity; set => value = Quantity; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string detail { get => Detail; set => value = Detail; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int price { get => Price; set => value = Price; }
+    }
+    public partial class Item
+    {
+        [JsonProperty("product", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Product { get; set; }
+        [JsonProperty("category", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Category { get; set; }
+        [JsonProperty("quantity", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int Quantity { get; set; }
+        [JsonProperty("detail", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Detail { get; set; }
+        [JsonProperty("price", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int Price { get; set; }
     }
 }

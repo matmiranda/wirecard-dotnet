81fc0565a3978cd77fa754494ee74f4eb91e78c5
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 19:18:34 -0200

    Update Fundinginstrument.cs

--- WirecardCSharp/WirecardCSharp/Models/Fundinginstrument.cs
+++ WirecardCSharp/WirecardCSharp/Models/Fundinginstrument.cs
@@ -1,14 +1,24 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class Fundinginstrument
+    public partial class Fundinginstrument
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Creditcard creditCard { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string method { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string brand { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Creditcard creditCard { get => CreditCard; set => value = CreditCard; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string method { get => Method; set => value = Method; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string brand { get => Brand; set => value = Brand; }
+    }
+    public partial class Fundinginstrument
+    {
+        [JsonProperty("creditCard", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Creditcard CreditCard { get; set; }
+        [JsonProperty("method", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Method { get; set; }
+        [JsonProperty("brand", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Brand { get; set; }
     }
 }

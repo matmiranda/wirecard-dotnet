12c10655d84d9c70f675a785ee6f5befb5441d26
Matheus Miranda <mat.miranda@outlook.com>
2018-11-06 23:30:20 -0200

    Update SalesFileResponse.cs

--- WirecardCSharp/WirecardCSharp/Models/Response/SalesFileResponse.cs
+++ WirecardCSharp/WirecardCSharp/Models/Response/SalesFileResponse.cs
@@ -1,10 +1,28 @@
-﻿namespace WirecardCSharp.Models
+using System;
+using Newtonsoft.Json;
+
+namespace WirecardCSharp.Models
 {
-    public class SalesFileResponse
+    public partial class SalesFileResponse
     {
-        public string id { get; set; }
-        public string date { get; set; }
-        public string type { get; set; }
-        public _Links _links { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string id { get => Id; set => value = Id; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string date { get => Date; set => value = Date; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string type { get => Type; set => value = Type; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public _Links _links { get => _Links; set => value = _Links; }
+    }
+    public partial class SalesFileResponse
+    {
+        [JsonProperty("id", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Id { get; set; }
+        [JsonProperty("date", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Date { get; set; }
+        [JsonProperty("type", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Type { get; set; }
+        [JsonProperty("_links", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public _Links _Links { get; set; }
     }
 }

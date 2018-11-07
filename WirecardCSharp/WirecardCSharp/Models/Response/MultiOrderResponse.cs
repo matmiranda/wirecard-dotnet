280a2eecc790c774e641f8afbe1a8baa4a7e09f6
Matheus Miranda <mat.miranda@outlook.com>
2018-11-06 23:26:56 -0200

    Update MultiOrderResponse.cs

--- WirecardCSharp/WirecardCSharp/Models/Response/MultiOrderResponse.cs
+++ WirecardCSharp/WirecardCSharp/Models/Response/MultiOrderResponse.cs
@@ -1,17 +1,45 @@
-﻿using System;
+using System;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 
 namespace WirecardCSharp.Models
 {
-    public class MultiOrderResponse
+    public partial class MultiOrderResponse
     {
-        public string id { get; set; }
-        public string ownId { get; set; }
-        public string status { get; set; }
-        public DateTime createdAt { get; set; }
-        public string updatedAt { get; set; }
-        public Amount amount { get; set; }
-        public List<Order> orders { get; set; }
-        public _Links _links { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string id { get => Id; set => value = Id; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string ownId { get => OwnId; set => value = OwnId; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string status { get => Status; set => value = Status; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public DateTime createdAt { get => CreatedAt; set => value = CreatedAt; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string updatedAt { get => UpdatedAt; set => value = UpdatedAt; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Amount amount { get => Amount; set => value = Amount; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public List<Order> orders { get => Orders; set => value = Orders; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public _Links _links { get => _Links; set => value = _Links; }
+    }
+    public partial class MultiOrderResponse
+    {
+        [JsonProperty("id", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Id { get; set; }
+        [JsonProperty("ownId", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string OwnId { get; set; }
+        [JsonProperty("status", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Status { get; set; }
+        [JsonProperty("createdAt", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public DateTime CreatedAt { get; set; }
+        [JsonProperty("updatedAt", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string UpdatedAt { get; set; }
+        [JsonProperty("amount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Amount Amount { get; set; }
+        [JsonProperty("orders", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public List<Order> Orders { get; set; }
+        [JsonProperty("_links", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public _Links _Links { get; set; }
     }
 }

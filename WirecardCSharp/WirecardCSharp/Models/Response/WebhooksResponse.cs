0b95346d4f1f62d7a733177059d58f3e3bc31cb8
Matheus Miranda <mat.miranda@outlook.com>
2018-11-06 23:31:47 -0200

    Update WebhooksResponse.cs

--- WirecardCSharp/WirecardCSharp/Models/Response/WebhooksResponse.cs
+++ WirecardCSharp/WirecardCSharp/Models/Response/WebhooksResponse.cs
@@ -1,9 +1,17 @@
-﻿using System.Collections.Generic;
+using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace WirecardCSharp.Models
 {
-    public class WebhooksResponse
+    public partial class WebhooksResponse
     {
-        public List<Webhook> webhooks { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public List<Webhook> webhooks { get => WebHooks; set => value = WebHooks; }
+    }
+    public partial class WebhooksResponse
+    {
+        [JsonProperty("webhooks", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public List<Webhook> WebHooks { get; set; }
     }
 }

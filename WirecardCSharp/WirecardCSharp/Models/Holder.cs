ed748233df5acd0ddf9ffc103b300894f2d6eec6
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 19:45:46 -0200

    Update Holder.cs

--- WirecardCSharp/WirecardCSharp/Models/Holder.cs
+++ WirecardCSharp/WirecardCSharp/Models/Holder.cs
@@ -1,20 +1,36 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class Holder
+    public partial class Holder
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string fullname { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string birthdate { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Taxdocument taxDocument { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Phone phone { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Billingaddress billingAddress { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public bool thirdParty { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string fullname { get => FullName; set => value = FullName; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string birthdate { get => BirthDate; set => value = BirthDate; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Taxdocument taxDocument { get => TaxDocument; set => value = TaxDocument; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Phone phone { get => Phone; set => value = Phone; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Billingaddress billingAddress { get => BillingAddress; set => value = BillingAddress; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public bool thirdParty { get => ThirdParty; set => value = ThirdParty; }
+    }
+    public partial class Holder
+    {
+        [JsonProperty("fullname", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string FullName { get; set; }
+        [JsonProperty("birthdate", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string BirthDate { get; set; }
+        [JsonProperty("taxDocument", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Taxdocument TaxDocument { get; set; }
+        [JsonProperty("phone", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Phone Phone { get; set; }
+        [JsonProperty("billingAddress", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Billingaddress BillingAddress { get; set; }
+        [JsonProperty("thirdParty", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public bool ThirdParty { get; set; }
     }
 }

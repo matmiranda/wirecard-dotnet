108c600ed52b74245f082c813168376c70de1dc9
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 19:58:07 -0200

    Update Mainactivity.cs

--- WirecardCSharp/WirecardCSharp/Models/Mainactivity.cs
+++ WirecardCSharp/WirecardCSharp/Models/Mainactivity.cs
@@ -1,12 +1,20 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class Mainactivity
+    public partial class Mainactivity
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string cnae { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string description { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string cnae { get => Cnae; set => value = Cnae; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string description { get => Description; set => value = Description; }
+    }
+    public partial class Mainactivity
+    {
+        [JsonProperty("cnae", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Cnae { get; set; }
+        [JsonProperty("description", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Description { get; set; }
     }
 }

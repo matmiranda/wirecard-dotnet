ac07b5050426918f06877ab9ee96187e48eb633d
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 16:56:48 -0200

    Update Person.cs

--- WirecardCSharp/WirecardCSharp/Models/Person.cs
+++ WirecardCSharp/WirecardCSharp/Models/Person.cs
@@ -1,24 +1,44 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class Person
+    public partial class Person
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string name { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string lastName { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string birthDate { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Taxdocument taxDocument { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Identitydocument identityDocument { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Phone phone { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Address address { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Parentsname parentsName { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string name { get => Name; set => value = Name; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string lastName { get => LastName; set => value = LastName; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string birthDate { get => BirthDate; set => value = BirthDate; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Taxdocument taxDocument { get => TaxDocument; set => value = TaxDocument; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Identitydocument identityDocument { get => IdentityDocument; set => value = IdentityDocument; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Phone phone { get => Phone; set => value = Phone; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Address address { get => Address; set => value = Address; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Parentsname parentsName { get => ParentsName; set => value = ParentsName; }
+    }
+    public partial class Person
+    {
+        [JsonProperty("name", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Name { get; set; }
+        [JsonProperty("lastName", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string LastName { get; set; }
+        [JsonProperty("birthDate", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string BirthDate { get; set; }
+        [JsonProperty("taxDocument", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Taxdocument TaxDocument { get; set; }
+        [JsonProperty("identityDocument", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Identitydocument IdentityDocument { get; set; }
+        [JsonProperty("phone", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Phone Phone { get; set; }
+        [JsonProperty("address", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Address Address { get; set; }
+        [JsonProperty("parentsName", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Parentsname ParentsName { get; set; }
     }
 }

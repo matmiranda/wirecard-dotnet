527874f8c0ce28d5fac8c19cff8db1953ffaaec6
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 18:38:32 -0200

    Update Checkoutpreferences.cs

--- WirecardCSharp/WirecardCSharp/Models/Checkoutpreferences.cs
+++ WirecardCSharp/WirecardCSharp/Models/Checkoutpreferences.cs
@@ -1,13 +1,21 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 
 namespace WirecardCSharp.Models
 {
-    public class Checkoutpreferences
+    public partial class Checkoutpreferences
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public List<Installment> installments { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Redirecturls redirectUrls { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public List<Installment> installments { get => Installments; set => value = Installments; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Redirecturls redirectUrls { get => RedirectUrls; set => value = RedirectUrls; }
+    }
+    public partial class Checkoutpreferences
+    {
+        [JsonProperty("installments", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public List<Installment> Installments { get; set; }
+        [JsonProperty("redirectUrls", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Redirecturls RedirectUrls { get; set; }
     }
 }

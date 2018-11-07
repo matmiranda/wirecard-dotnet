bba2cdc25ba2bc1b56a946ce87cf012166f957f7
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 16:52:52 -0200

    Update Setpassword.cs

--- WirecardCSharp/WirecardCSharp/Models/Setpassword.cs
+++ WirecardCSharp/WirecardCSharp/Models/Setpassword.cs
@@ -1,10 +1,16 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class Setpassword
+    public partial class Setpassword
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string href { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string href { get => Href; set => value = Href; }
+    }
+    public partial class Setpassword
+    {
+        [JsonProperty("href", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Href { get; set; }
     }
 }

e6d362d33df90c57b976aa62777b015764dda332
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 19:41:22 -0200

    Update Geolocation.cs

--- WirecardCSharp/WirecardCSharp/Models/Geolocation.cs
+++ WirecardCSharp/WirecardCSharp/Models/Geolocation.cs
@@ -1,12 +1,20 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class Geolocation
+    public partial class Geolocation
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public float latitude { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public float longitude { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public float latitude { get => Latitude; set => value = Latitude; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public float longitude { get => Longitude; set => value = Longitude; }
+    }
+    public partial class Geolocation
+    {
+        [JsonProperty("latitude", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public float Latitude { get; set; }
+        [JsonProperty("longitude", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public float Longitude { get; set; }
     }
 }

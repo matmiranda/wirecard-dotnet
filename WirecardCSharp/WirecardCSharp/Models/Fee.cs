87cbc28d9ea9bfdbfda56aded145fed93752ee4a
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 19:16:14 -0200

    Update Fee.cs

--- WirecardCSharp/WirecardCSharp/Models/Fee.cs
+++ WirecardCSharp/WirecardCSharp/Models/Fee.cs
@@ -1,12 +1,20 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class Fee
+    public partial class Fee
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string type { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int amount { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string type { get => Type; set => value = Type; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int amount { get => Amount; set => value = Amount; }
+    }
+    public partial class Fee
+    {
+        [JsonProperty("type", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Type { get; set; }
+        [JsonProperty("amount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int Amount { get; set; }
     }
 }

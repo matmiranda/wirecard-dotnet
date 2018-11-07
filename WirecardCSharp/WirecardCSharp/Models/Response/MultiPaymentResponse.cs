cf6972cab67d2a3f73e88c794b421c6fe02beded
Matheus Miranda <mat.miranda@outlook.com>
2018-11-06 23:27:21 -0200

    Update MultiPaymentResponse.cs

--- WirecardCSharp/WirecardCSharp/Models/Response/MultiPaymentResponse.cs
+++ WirecardCSharp/WirecardCSharp/Models/Response/MultiPaymentResponse.cs
@@ -1,18 +1,49 @@
-﻿using System;
+using System;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 
 namespace WirecardCSharp.Models
 {
-    public class MultiPaymentResponse
+    public partial class MultiPaymentResponse
     {
-        public string id { get; set; }
-        public string status { get; set; }
-        public Amount amount { get; set; }
-        public int installmentCount { get; set; }
-        public List<Payment> payments { get; set; }
-        public _Links _links { get; set; }
-        public string description { get; set; }
-        public DateTime createdAt { get; set; }
-        public DateTime updatedAt { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string id { get => Id; set => value = Id; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string status { get => Status; set => value = Status; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Amount amount { get => Amount; set => value = Amount; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int installmentCount { get => InstallmentCount; set => value = InstallmentCount; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public List<Payment> payments { get => Payments; set => value = Payments; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public _Links _links { get => _Links; set => value = _Links; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string description { get => Description; set => value = Description; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public DateTime createdAt { get => CreatedAt; set => value = CreatedAt; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public DateTime updatedAt { get => UpdatedAt; set => value = UpdatedAt; }
+    }
+    public partial class MultiPaymentResponse
+    {
+        [JsonProperty("id", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Id { get; set; }
+        [JsonProperty("status", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Status { get; set; }
+        [JsonProperty("amount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Amount Amount { get; set; }
+        [JsonProperty("installmentCount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int InstallmentCount { get; set; }
+        [JsonProperty("payments", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public List<Payment> Payments { get; set; }
+        [JsonProperty("_links", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public _Links _Links { get; set; }
+        [JsonProperty("description", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Description { get; set; }
+        [JsonProperty("createdAt", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public DateTime CreatedAt { get; set; }
+        [JsonProperty("updatedAt", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public DateTime UpdatedAt { get; set; }
     }
 }

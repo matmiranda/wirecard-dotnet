734bb2c552e0b94a9306e7d41744bd721475517e
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 16:55:27 -0200

    Update Receiver.cs

--- WirecardCSharp/WirecardCSharp/Models/Receiver.cs
+++ WirecardCSharp/WirecardCSharp/Models/Receiver.cs
@@ -1,16 +1,28 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class Receiver
+    public partial class Receiver
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string type { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public bool feePayor { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Moipaccount moipAccount { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public Amount amount { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string type { get => Type; set => value = Type; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public bool feePayor { get => FeePayor; set => value = FeePayor; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Moipaccount moipAccount { get => MoipAccount; set => value = MoipAccount; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public Amount amount { get => Amount; set => value = Amount; }
+    }
+    public partial class Receiver
+    {
+        [JsonProperty("type", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Type { get; set; }
+        [JsonProperty("feePayor", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public bool FeePayor { get; set; }
+        [JsonProperty("moipAccount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Moipaccount MoipAccount { get; set; }
+        [JsonProperty("amount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Amount Amount { get; set; }
     }
 }

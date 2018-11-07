6c95fd3b6cdaf93b9d3eeb812a30ac842c867e0c
Matheus Miranda <mat.miranda@outlook.com>
2018-11-07 18:05:15 -0200

    Update Amount.cs

--- WirecardCSharp/WirecardCSharp/Models/Amount.cs
+++ WirecardCSharp/WirecardCSharp/Models/Amount.cs
@@ -1,33 +1,62 @@
-﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json;
 
 namespace WirecardCSharp.Models
 {
-    public class Amount
+    public partial class Amount
     {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string currency { get; set; }
-        public Subtotals subtotals { get; set; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public string currency { get => Currency; set => value = Currency; }
+        public Subtotals subtotals { get => Subtotals; set => value = Subtotals; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int @fixed { get => Fixed; set => value = Fixed; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int percentual { get => Percentual; set => value = Percentual; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int paid { get => Paid; set => value = Paid; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int total { get => Total; set => value = Total; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int fees { get => Fees; set => value = Fees; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int refunds { get => Refunds; set => value = Refunds; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int liquid { get => Liquid; set => value = Liquid; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int otherReceivers { get => OtherReceivers; set => value = OtherReceivers; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int addition { get => Addition; set => value = Addition; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int deduction { get => Deduction; set => value = Deduction; }
+        [Obsolete("Utilize a propriedade que inicia com a letra maiúscula. Essa deixará de existir a partir da versão 2.0.0.")]
+        public int gross { get => Gross; set => value = Gross; }
+    }
+    public partial class Amount
+    {
+        [JsonProperty("currency", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Currency { get; set; }
+        public Subtotals Subtotals { get; set; }
         [JsonProperty("fixed", DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int @fixed { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int percentual { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int paid { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int total { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int fees { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int refunds { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int liquid { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int otherReceivers { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int addition { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int deduction { get; set; }
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int gross { get; set; }
+        public int Fixed { get; set; }
+        [JsonProperty("percentual", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int Percentual { get; set; }
+        [JsonProperty("paid", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int Paid { get; set; }
+        [JsonProperty("total", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int Total { get; set; }
+        [JsonProperty("fees", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int Fees { get; set; }
+        [JsonProperty("refunds", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int Refunds { get; set; }
+        [JsonProperty("liquid", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int Liquid { get; set; }
+        [JsonProperty("otherReceivers", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int OtherReceivers { get; set; }
+        [JsonProperty("addition", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int Addition { get; set; }
+        [JsonProperty("deduction", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int Deduction { get; set; }
+        [JsonProperty("gross", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int Gross { get; set; }
     }
 }

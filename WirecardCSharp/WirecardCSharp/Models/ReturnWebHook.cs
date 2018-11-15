a55a0fb606610545a4afec3990d95fafe6d795db
Matheus Miranda <mat.miranda@outlook.com>
2018-11-15 12:34:19 -0200

    Add files via upload

---
+++ WirecardCSharp/WirecardCSharp/Models/ReturnWebHook.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace WirecardCSharp.Models
+{
+    public class ReturnWebHook
+    {
+        [JsonProperty("date", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Date { get; set; }
+        [JsonProperty("env", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Env { get; set; }
+        [JsonProperty("event", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Event { get; set; }
+        [JsonProperty("resource", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Resource Resource { get; set; }
+    }
+}

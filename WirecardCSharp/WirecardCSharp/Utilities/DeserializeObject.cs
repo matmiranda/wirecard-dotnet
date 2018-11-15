0d55facc5eb9b03cfb48fc6b85f0dcc804b0620f
Matheus Miranda <mat.miranda@outlook.com>
2018-11-15 12:36:32 -0200

    Add files via upload

---
+++ WirecardCSharp/WirecardCSharp/Utilities/DeserializeObject.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using WirecardCSharp.Models;
+
+namespace WirecardCSharp.Utilities
+{
+    internal class DeserializeObject
+    {
+        //convert json to object
+        internal static ReturnWebHook WebHook(string json)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<ReturnWebHook>(json);
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}

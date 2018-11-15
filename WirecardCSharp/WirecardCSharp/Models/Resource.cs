eb371ff5e89cdbf117f1beb336c2630fcea43a94
Matheus Miranda <mat.miranda@outlook.com>
2018-11-15 12:35:00 -0200

    Add files via upload

---
+++ WirecardCSharp/WirecardCSharp/Models/Resource.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace WirecardCSharp.Models
+{
+    public class Resource
+    {
+        [JsonProperty("order", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Order Order { get; set; }
+    }
+}

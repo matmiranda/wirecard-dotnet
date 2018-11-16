7f26851e134374675b6c136f6fce55b53287717d
Matheus Miranda <mat.miranda@outlook.com>
2018-11-16 18:57:14 -0200

    Add files via upload

---
+++ WirecardCSharp/WirecardCSharp/Models/Createdat.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace WirecardCSharp.Models
+{
+    public class Createdat
+    {
+        [JsonProperty("date", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public DateTime Date { get; set; }
+    }
+}

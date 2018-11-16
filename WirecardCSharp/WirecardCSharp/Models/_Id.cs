ad3ac4034c507183e6a5a1c0be329ba8130426e3
Matheus Miranda <mat.miranda@outlook.com>
2018-11-16 18:50:05 -0200

    Add files via upload

---
+++ WirecardCSharp/WirecardCSharp/Models/_Id.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace WirecardCSharp.Models
+{
+    public class _Id
+    {
+        [JsonProperty("oid", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Oid { get; set; }
+    }
+}

50845bf85395a57886455590dbcd04866ec04d2e
Matheus Miranda <mat.miranda@outlook.com>
2018-11-17 00:27:46 -0200

    Update Multiorder.cs

--- WirecardCSharp/WirecardCSharp/Models/Multiorder.cs
+++ WirecardCSharp/WirecardCSharp/Models/Multiorder.cs
@@ -1,5 +1,6 @@
 using System;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace WirecardCSharp.Models
 {
@@ -12,5 +13,21 @@ namespace WirecardCSharp.Models
     {
         [JsonProperty("href", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public string Href { get; set; }
+        [JsonProperty("_links", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public _Links _Links { get; set; }
+        [JsonProperty("amount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Amount Amount { get; set; }
+        [JsonProperty("createdAt", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public DateTime CreatedAt { get; set; }
+        [JsonProperty("id", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Id { get; set; }
+        [JsonProperty("orders", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public List<Order> Orders { get; set; }
+        [JsonProperty("ownId", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string OwnId { get; set; }
+        [JsonProperty("status", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Status { get; set; }
+        [JsonProperty("updatedAt", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string UpdatedAt { get; set; }
     }
 }

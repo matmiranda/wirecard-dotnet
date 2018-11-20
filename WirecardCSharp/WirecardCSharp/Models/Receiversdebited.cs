acc6e1e6394f4f015967a14bd7475a132c8aea29
Matheus Miranda <mat.miranda@outlook.com>
2018-11-20 13:30:50 -0200

    Add files via upload

---
+++ WirecardCSharp/WirecardCSharp/Models/Receiversdebited.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace WirecardCSharp.Models
+{
+    public class Receiversdebited
+    {
+        [JsonProperty("Amount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int amount { get; set; }
+        [JsonProperty("moipAccount", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string MoipAccount { get; set; }
+    }
+}

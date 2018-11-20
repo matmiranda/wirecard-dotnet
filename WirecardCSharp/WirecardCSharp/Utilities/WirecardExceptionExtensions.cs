03cf1f079a0f2d8ef386a5176c194a03b0ea45aa
Fernando Tangoda <fernandotangoda@gmail.com>
2018-11-20 14:06:56 -0200

    Extension para obter os erros como texto

---
+++ WirecardCSharp/WirecardCSharp/Utilities/WirecardExceptionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WirecardCSharp.Exception;
+
+namespace WirecardCSharp
+{
+    public static class WirecardExceptionExtensions
+    {
+        public static string GetExceptionText(this WirecardException we)
+        {
+            var sb = new StringBuilder();
+
+            if (we != null)
+            {
+                if (we.wirecardError == null || !we.wirecardError.errors.Any())
+                    return sb.ToString();
+
+                foreach (var error in we.wirecardError.errors)
+                    sb.AppendLine($"{error.description} ({error.code})");
+            }
+
+            return sb.ToString();
+        }
+    }
+}

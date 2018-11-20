f8b5e322738600f9b2738161ce12c0805c2571db
Matheus Miranda <mat.miranda@outlook.com>
2018-11-20 15:24:12 -0200

    Downgrade .NET Standard 1.2

---
+++ WirecardCSharp/Models/Updatedat.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace WirecardCSharp.Models
+{
+    public class Updatedat
+    {
+        [JsonProperty("$date", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public DateTime Date { get; set; }
+    }
+}
---
+++ WirecardCSharp/Utilities/Utilities.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using WirecardCSharp.Models;
+
+namespace WirecardCSharp
+{
+    public static class Utilities
+    {
+        /// <summary> Converte json para objeto - deserialize to a object </summary>
+        /// <param name="json">Retorno json</param>
+        /// <returns></returns>
+        public static ReturnWebHook DeserializeWebHook(string json)
+        {
+            try
+            {
+                JsonSerializerSettings setting = new JsonSerializerSettings
+                {
+                    MetadataPropertyHandling = MetadataPropertyHandling.Ignore
+                };
+                return JsonConvert.DeserializeObject<ReturnWebHook>(json, setting);
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}

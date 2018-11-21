1a1ea5f20027a2b71960dbdc5a3dd09c21662e01
Matheus Miranda <mat.miranda@outlook.com>
2018-11-21 14:09:35 -0200

    Update PaymentRequest.cs

--- WirecardCSharp/Models/Request/PaymentRequest.cs
+++ WirecardCSharp/Models/Request/PaymentRequest.cs
@@ -8,9 +8,13 @@ namespace WirecardCSharp.Models
         public int InstallmentCount { get; set; }
         [JsonProperty("statementDescriptor", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public string StatementDescriptor { get; set; }
+        [JsonProperty("delayCapture", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public bool DelayCapture { get; set; }
         [JsonProperty("fundingInstrument", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public Fundinginstrument FundingInstrument { get; set; }
         [JsonProperty("device", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public Device Device { get; set; }
+        [JsonProperty("fingerprint", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Fingerprint { get; set; }
     }
 }

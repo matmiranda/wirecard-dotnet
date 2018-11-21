c160d84f5827b9cc7e6f0b1c8c2ae402dcd8e019
Matheus Miranda <mat.miranda@outlook.com>
2018-11-21 21:35:01 -0200

    Update Utilities.cs

--- WirecardCSharp/Utilities/Utilities.cs
+++ WirecardCSharp/Utilities/Utilities.cs
@@ -1,5 +1,9 @@
-﻿using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.Linq;
+using Newtonsoft.Json;
 using WirecardCSharp.Models;
+using WirecardCSharp.Exception;
 
 namespace WirecardCSharp
 {
@@ -17,6 +21,45 @@ namespace WirecardCSharp
                     MetadataPropertyHandling = MetadataPropertyHandling.Ignore
                 };
                 return JsonConvert.DeserializeObject<ReturnWebHook>(json, setting);
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+        }
+        /// <summary>
+        /// Obter os erros como texto - Get errors as text
+        /// </summary>
+        /// <param name="we">WirecardException</param>
+        /// <returns></returns>
+        public static string GetExceptionText(this WirecardException we)
+        {
+            var sb = new StringBuilder();
+
+            if (we != null)
+            {
+                if (we.wirecardError == null || !we.wirecardError.errors.Any())
+                    return sb.ToString();
+
+                foreach (var error in we.wirecardError.errors)
+                    sb.AppendLine($"{error.description} ({error.code})");
+            }
+            return sb.ToString();
+        }
+        /// <summary>
+        /// Solicita Permissões de Acesso ao Usuário - Request User Access Permissions
+        /// </summary>
+        /// <param name="response_type">Define o tipo de resposta a ser obtido. Valores possíveis: code</param>
+        /// <param name="client_id">Identificador único do aplicativo que será realizada a solicitação. Exemplo: APP-DVLJHW59IKOS</param>
+        /// <param name="redirect_uri">URI de redirecionamento do cliente. ATENÇÂO: deve ser a mesma url cadastrada ao criar o app.</param>
+        /// <param name="scope">Permissões que deseja. Valores possíveis: RECEIVE_FUNDS, REFUND, MANAGE_ACCOUNT_INFO, RETRIEVE_FINANCIAL_INFO, TRANSFER_FUNDS, DEFINE_PREFERENCES</param>
+        /// <returns></returns>
+        public static string RequestUserAccessPermissions(string response_type, string client_id, string redirect_uri, string scope)
+        {
+            try
+            {
+                string _params = $"oauth/authorize?response_type={response_type}&client_id={client_id}&redirect_uri={redirect_uri}&scope={scope}";
+                return new Uri(new Uri("https://connect-sandbox.moip.com.br/"), _params).AbsoluteUri;
             }
             catch (System.Exception ex)
             {

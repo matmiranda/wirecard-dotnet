8c4bf033ac6f7f0050183ec1ede5fa22e7547b50
Matheus Miranda <mat.miranda@outlook.com>
2018-11-27 00:15:43 -0200

    Update ClassicAccountRequest.cs

--- WirecardCSharp/Models/Request/ClassicAccountRequest.cs
+++ WirecardCSharp/Models/Request/ClassicAccountRequest.cs
@@ -12,7 +12,11 @@ namespace WirecardCSharp.Models
         public Company Company { get; set; }
         [JsonProperty("businessSegment", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public Businesssegment BusinessSegment { get; set; }
+        [JsonProperty("site", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Site { get; set; }
         [JsonProperty("type", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public string Type { get; set; }
+        [JsonProperty("tosAcceptance", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public TosAcceptance TosAcceptance { get; set; }
     }
 }

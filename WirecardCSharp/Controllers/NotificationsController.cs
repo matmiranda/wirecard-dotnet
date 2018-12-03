369915239a0048eca5f205c053f1771174a49f6a
Matheus Miranda <mat.miranda@outlook.com>
2018-12-03 20:04:28 -0200

    Update NotificationsController.cs

--- WirecardCSharp/Controllers/NotificationsController.cs
+++ WirecardCSharp/Controllers/NotificationsController.cs
@@ -1,4 +1,4 @@
-﻿using System.Net;
+using System.Net;
 using System.Text;
 using Newtonsoft.Json;
 using System.Net.Http;
@@ -37,6 +37,30 @@ namespace WirecardCSharp.Controllers
         /// <param name="body"></param>
         /// <returns></returns>
         public async Task<NotificationResponse> CreatAccountWirecard(NotificationRequest body)
+        {
+            StringContent stringContent = new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await ClientInstance.PostAsync("v2/preferences/notifications", stringContent);
+            if (!response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                WirecardException.WirecardError wirecardException = WirecardException.DeserializeObject(content);
+                throw new WirecardException(wirecardException, "HTTP Response Not Success", content, (int)response.StatusCode);
+            }
+            try
+            {
+                return JsonConvert.DeserializeObject<NotificationResponse>(await response.Content.ReadAsStringAsync());
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+        }
+        /// <summary>
+        /// Criar Preferência de Notificação para App - Create Notification Preference for App
+        /// </summary>
+        /// <param name="body"></param>
+        /// <returns></returns>
+        public async Task<NotificationResponse> CreateApp(NotificationRequest body)
         {
             StringContent stringContent = new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, "application/json");
             HttpResponseMessage response = await ClientInstance.PostAsync("v2/preferences/notifications", stringContent);

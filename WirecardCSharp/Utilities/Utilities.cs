c5b46fdab458e3d2012b239a1b5eae0e497808ff
Matheus Miranda <mat.miranda@outlook.com>
2018-12-09 17:02:04 -0200

    Update Utilities.cs

--- WirecardCSharp/Utilities/Utilities.cs
+++ WirecardCSharp/Utilities/Utilities.cs
@@ -2,6 +2,7 @@ using System;
 using System.Text;
 using System.Linq;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using WirecardCSharp.Models;
 using WirecardCSharp.Exception;
 
@@ -65,6 +66,29 @@ namespace WirecardCSharp
             {
                 throw ex;
             }
+        }
+
+        public static void Rename(this JToken token, string newName)
+        {
+            if (token == null) throw new ArgumentNullException("token", "Cannot rename a null token");
+
+            JProperty property;
+
+            if (token.Type == JTokenType.Property)
+            {
+                if (token.Parent == null) throw new InvalidOperationException("Cannot rename a property with no parent");
+
+                property = (JProperty)token;
+            }
+            else
+            {
+                if (token.Parent == null || token.Parent.Type != JTokenType.Property) throw new InvalidOperationException("This token's parent is not a JProperty; cannot rename");
+
+                property = (JProperty)token.Parent;
+            }
+
+            var newProperty = new JProperty(newName, property.Value);
+            property.Replace(newProperty);
         }
     }
 }

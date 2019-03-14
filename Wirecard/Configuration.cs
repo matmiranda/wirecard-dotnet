4112d547c59e0dd681dbc37d3e49206ceddf72c0
Matheus Miranda <mat.miranda@outlook.com>
2019-03-14 18:26:40 -0300

    Nova propriedade: PayOnlineBankDebitItau

--- Wirecard/Configuration.cs
+++ Wirecard/Configuration.cs
@@ -95,7 +95,7 @@ namespace Wirecard
             //return FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion;
 
             //for .NET Standard 1.2 - write the version number manually.
-            return "3.0.9";
+            return "3.1.0";
         }
     }
 }

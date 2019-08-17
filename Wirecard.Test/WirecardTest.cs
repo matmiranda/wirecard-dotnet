88cb19096b2ac79dff9135294d634230b9717742
Matheus Miranda <mat.miranda@outlook.com>
2019-08-17 13:06:20 -0300

    Add Wirecard.Test

---
+++ Wirecard.Test/WirecardTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Wirecard.Test
+{
+    class WirecardTest
+    {
+        [Test]
+        public void TesteComAccessTokenDiferente()
+        {
+            var instancia1 = new WirecardClient(Environments.SANDBOX, "123");
+            var instancia2 = new WirecardClient(Environments.SANDBOX, "456");
+
+            var result1 = instancia1.ClassicAccount.GetPublickey().Result.keys.BasicAuth.Token;
+            var result2 = instancia2.ClassicAccount.GetPublickey().Result.keys.BasicAuth.Token;
+        }
+    }
+}
